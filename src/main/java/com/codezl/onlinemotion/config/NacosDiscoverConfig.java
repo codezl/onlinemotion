c2273ec1ef6d44353f2d0c90db0b3b1ff553afae
codezl <56688336+codezl@users.noreply.github.com>
2022-06-23 14:17:57 +0800

    上传自身定位以及获取连线好友坐标

---
+++ src/main/java/com/codezl/onlinemotion/config/NacosDiscoverConfig.java
@@ -0,0 +1,16 @@
+package com.codezl.onlinemotion.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: code-zl
+ * @Date: 2022/06/22/10:30
+ * @Description:
+ */
+@EnableDiscoveryClient
+@Configuration
+public class NacosDiscoverConfig {
+}
--- src/main/java/com/codezl/onlinemotion/controller/OnlineConnectController.java
+++ src/main/java/com/codezl/onlinemotion/controller/OnlineConnectController.java
@@ -1,8 +1,10 @@
 package com.codezl.onlinemotion.controller;
 
+import com.codezl.onlinemotion.socket.WebSocketServerHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,9 +21,10 @@ public class OnlineConnectController {
     @Autowired
     StringRedisTemplate redisTemplate;
 
-    @RequestMapping("connect2one")
+    @RequestMapping(value = "connect2one",method = RequestMethod.POST)
     public void connect2one(String toUser) {
-        String localUser = "19";
+        String localUser = "connect";
         redisTemplate.opsForValue().set("connect2one"+localUser,toUser);
+        WebSocketServerHandler.onlineWs.get("/z");
     }
 }
